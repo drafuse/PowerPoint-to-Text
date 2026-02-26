4e74e486abc85523710a2af4cdec3e4d714733d1
d_raf <d_rafuse2@hotmail.com>
2026-02-26 16:19:34 -0300

    Remove icon flag from build script

--- build_exe.ps1
+++ build_exe.ps1
@@ -7,9 +7,6 @@ $ErrorActionPreference = "Stop"
 if (!(Test-Path ".\venv\Scripts\python.exe")) {
     throw "Virtual environment python not found at .\venv\Scripts\python.exe"
 }
-if (!(Test-Path ".\assets\app_icon.ico")) {
-    throw "Icon file not found at .\assets\app_icon.ico"
-}
 
 Write-Host "Installing/updating build dependencies..."
 .\venv\Scripts\python.exe -m pip install --upgrade pip pyinstaller python-pptx
@@ -19,7 +16,6 @@ Write-Host "Building standalone executable..."
     --onefile `
     --noconsole `
     --name $AppName `
-    --icon .\assets\app_icon.ico `
     --clean `
     .\pptx_to_text.py
 
